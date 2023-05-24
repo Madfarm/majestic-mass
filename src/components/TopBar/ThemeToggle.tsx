06cfbb7f67cd57bcdecc568de923cc12b6eed876
AnthonyScott <anthony.scott1005@gmail.com>
2023-05-24 18:09:06 -0400

    Update theme toggle button

---
+++ src/components/TopBar/ThemeToggle.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react"
+import './ThemeToggle.css'
+
+
+export default function ThemeToggle() {
+    //For client/server mismatch
+    const [isMounted, setIsMounted] = useState(false);
+
+    //Turning Theme into state and toggling it
+    const [theme, setTheme] = useState('light');
+    const toggleTheme = () => {
+        if (theme === 'light') {
+            setTheme('dark');
+        } else {
+            setTheme('light');
+        }
+    }
+
+    //Whenever theme state changes, document.body's class does too
+    useEffect(() => {
+        document.body.className = theme;
+    }, [theme])
+
+    //Ensure component is mounted
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if(!isMounted){
+        return null;
+    }
+
+    return (
+        <div>
+            <button onClick={toggleTheme}>
+                {theme === "light" ? "🌙" : "🌞"}
+            </button>
+        </div>
+    )
+}
