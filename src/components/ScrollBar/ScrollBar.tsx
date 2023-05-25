89d9873235649b02e4869dc21c4c8536eee579da
AnthonyScott <anthony.scott1005@gmail.com>
2023-05-25 17:44:36 -0400

    Add basic styles to side nav

--- src/components/ScrollBar/ScrollBar.tsx
+++ src/components/ScrollBar/ScrollBar.tsx
@@ -1,12 +1,34 @@
 import './ScrollBar.css'
+import { Link } from 'react-scroll';
 
-export default function ScrollBar(){
+export default function ScrollBar() {
     return (
-        <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+        <ul className='list-container'>
+            <li>
+                <Link activeClass="active" smooth spy to="about">
+                    ABOUT
+                </Link>
+            </li>
+            <li>
+                <Link activeClass="active" smooth spy to="projects">
+                    PROJECTS
+                </Link>
+                <ul>
+                    <li>
+                        <Link activeClass="active" smooth spy to="portfolios"> Past Portfolios</Link>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <Link activeClass="active" smooth spy to="skills">
+                    SKILLS
+                </Link>
+            </li>
+            <li>
+                <Link activeClass="active" smooth spy to="contact">
+                    CONTACT ME
+                </Link>
+            </li>
         </ul>
     )
 }
