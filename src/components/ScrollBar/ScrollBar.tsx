9f77c010a13975fb3e83c7358a96321728cbdf4a
AnthonyScott <anthony.scott1005@gmail.com>
2023-05-25 17:26:53 -0400

    Add reactscroll and scaffold scroll side bar

---
+++ src/components/ScrollBar/ScrollBar.tsx
@@ -0,0 +1,12 @@
+import './ScrollBar.css'
+
+export default function ScrollBar(){
+    return (
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    )
+}
