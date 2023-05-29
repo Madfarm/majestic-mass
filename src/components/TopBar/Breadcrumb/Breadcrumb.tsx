b2711703bc5900184b280a2afb04b45a202704b8
AnthonyScott <anthony.scott1005@gmail.com>
2023-05-29 19:22:17 -0400

    Add mobile breadcrumb

--- src/components/TopBar/Breadcrumb/Breadcrumb.tsx
+++ src/components/TopBar/Breadcrumb/Breadcrumb.tsx
@@ -1,8 +1,14 @@
 import { Link } from "react-scroll";
-
+import './Breadcrumb.css'
 
 export default function BreadCrumb(){
     return (
-        <Link className="breadcrumb" activeClass="activeBreadcrumb" smooth spy to="projects">Hats</Link>
+        <>
+        <Link className="breadcrumb" activeClass="activeBreadcrumb" smooth spy to="about">About</Link>
+        <Link className="breadcrumb" activeClass="activeBreadcrumb" smooth spy to="projects">Projects</Link>
+        <Link className="breadcrumb" activeClass="activeBreadcrumb" smooth spy to="portfolios">Portfolios</Link>
+        <Link className="breadcrumb" activeClass="activeBreadcrumb" smooth spy to="skills">Skills</Link>
+        <Link className="breadcrumb" activeClass="activeBreadcrumb" smooth spy to="contact">Contact Me</Link>
+        </>
     )
 }
